5389d98bc2c4f5882bd4581fe674f76b00894bbf
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-04 11:34:03 +0200

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,8 +1,11 @@
-module "storage_account" {
-  source = "./sa"
+resource "azurerm_storage_account" "main" {
+  name                     = "azurerm_storage_account"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-  name = "azurerm_storage_account"
-  resource_group_name = azurerm_resource_group.main.name
-
-  sku = "Standard_LRS"
+  tags = {
+    environment = "staging"
+  }
 }
