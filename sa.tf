65b5187ced5e5cdb4152223ed7e41772c9168586
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-04 12:27:24 +0200

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,5 +1,12 @@
+resource "random_id" "random_id" {
+  keepers = {
+    resource_group = azurerm_resource_group.main.name
+  }
+  byte_length = 8
+}
+
 resource "azurerm_storage_account" "main" {
-  name                     = "storageaccount"
+  name                     = "diag${random_id.random_id.hex}"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
