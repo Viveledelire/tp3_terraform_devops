9f1c2d7c2ca1e013223c4f5e3ed32ec1aa5a3dca
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-04 13:56:55 +0200

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -17,18 +17,19 @@ resource "azurerm_network_interface" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_public_ip" "main" {
+resource "azurerm_public_ip" "my_terraform_public_ip" {
     name                = "example-public-ip"
     location            = azurerm_resource_group.main.location
     resource_group_name = azurerm_resource_group.main.name
     allocation_method   = "Dynamic"
+}
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.main.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
