a6e5d7bbae649b350844032e24e7786e532c2f61
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-04 11:25:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
 resource "azurerm_resource_group" "main" {
-  name = "azurerm_resource_group"
-  location = "East US"
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
